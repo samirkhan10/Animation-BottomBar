5f0f4d8521f7844eca5b8f5cbb8ecb10cfdc879b
Samir Khan <pkan5320@gmail.com>
2023-06-28 14:43:06 +0530

    initial commit

---
+++ lib/CustomColorTheme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class CustomColorTheme {
+  static const Color activeNavigationBarColor = Color(0xFFFFFFFF);
+  static const Color notActiveNavigationBarColor = Color(0xFF1091EF);
+  static const Color bottomNavigationBarBackgroundColor = Color(0xFF987654);
+  static const Color activeNavigationBarColors = Color(0xFFEFEFEF);
+  static const Color color5 = Color(0xFF000000);
+}
